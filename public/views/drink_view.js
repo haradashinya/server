e7a2e4170ad09cbdb6cda8742054f8eff8d4c530
okamurayasuyuki <haradashinya@gmail.com>
2012-12-01 22:56:29 +0900

    fixed

--- public/views/drink_view.js
+++ public/views/drink_view.js
@@ -5,7 +5,7 @@ define(["zepto","underscore","backbone","lib/text!templates/drink_view.html","li
 			initialize:function(){
 				_.bindAll(this,"removeDrink","updateDrink","render");
 				this.model.bind("change",this.changed,this);
-				$("li .remove-drink").bind("tap",this.removeDrink());
+				$("li .remove-drink").bind("tap",this.removeDrink);
 
 			},
 			events: {
