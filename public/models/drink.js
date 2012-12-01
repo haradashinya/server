1902a5a1114a93d6dd983c674a8a17eaacd49986
okamrauyasuyuki <haradashinya@gmail.com>
2012-12-01 12:32:44 +0000

    bug fix: completed remove drink when user clicked

--- public/models/drink.js
+++ public/models/drink.js
@@ -1,8 +1,5 @@
 define(["zepto","underscore","backbone"],function($,_,Backbone){
 	var Drink = Backbone.Model.extend({
-		url:function(){
-				return "http://localhost:9393/users/" + window.uuid + "/drinks/11";
-		},
 		initialize:function(){
 			_.bindAll(this,"removeDrink");
 			this.on("destroy",this.removeDrink,this);
@@ -12,7 +9,7 @@ define(["zepto","underscore","backbone"],function($,_,Backbone){
 			var model = this.toJSON();
 			$.ajax({
 				type:"DELETE",
-				url:"http://54.248.226.131.3000/users/" + window.uuid + "/drinks/" + model._id,
+				url:"http://54.248.226.131:3000/users/" + window.uuid + "/drinks/" + model._id,
 				success:function(data){
 					console.log("removed drinks successfully");
 				}
