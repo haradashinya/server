2f33602f17d3d9cae7b5d4bd8a7750cac3ec4333
okamurayasuyuki <haradashinya@gmail.com>
2012-12-03 10:12:04 +0900

    fixed fabfile

--- fabfile.py
+++ fabfile.py
@@ -4,7 +4,6 @@ from fabric.operations import sudo
 from fabric.context_managers import cd
 import os
 
-
 HOME = os.getenv("HOME")
 env.user = "haradashinya"
 env.hosts = ["ec2-user@54.248.226.131"]
@@ -13,17 +12,17 @@ env.key_filename = ["%s/haradashinya.pem"%HOME]
 def host_type():
 	run("uname -s")
 
+# remote update
 def update():
 	with cd("/var/www/html/server"):
 		sudo("git pull")
 
+# git push and run git pull in the remote machine.
 def deploy():
 	local("git push")
 	with cd("/var/www/html/server"):
 		sudo("git pull origin master")
-		sudo("pwd")
-
-
+	local("terminal-notifier -message 'deploy success'")
 
 def push():
 	with cd("/var/www/html/server"):
