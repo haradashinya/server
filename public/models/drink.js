d03208e6e389097deab65f4637b2718c26ef4e4a
okamurayasuyuki <haradashinya@gmail.com>
2012-12-14 10:45:40 +0900

    work run localhost

--- public/models/drink.js
+++ public/models/drink.js
@@ -9,7 +9,7 @@ define(["zepto","underscore","backbone"],function($,_,Backbone){
 			var model = this.toJSON();
 			$.ajax({
 				type:"DELETE",
-				url:"http://54.248.226.131:3000/users/" + window.uuid + "/drinks/" + model._id,
+        url:window.baseURL +"/users/" + window.uuid + "/drinks/" + model._id,
 				success:function(data){
 					console.log("removed drinks successfully");
 
@@ -20,7 +20,7 @@ define(["zepto","underscore","backbone"],function($,_,Backbone){
 			var model = this.toJSON();
 			$.ajax({
 				type: "PUT",
-				url:"http://54.248.226.131:3000/users/" + window.uuid + "/drinks/" + model._id,
+        url:window.baseURL +"/users/" + window.uuid + "/drinks/" + model._id,
 				data: JSON.stringify(model),
 				success:function(data){
 					console.log("success update");
